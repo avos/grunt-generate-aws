ac01b8106149ccaf1b82866033f7de7cfa5b31f3
Chris Abrams <mail@chrisabrams.com>
2013-02-21 14:21:55 -0800

    Clear old junk.

--- tasks/grunt-cp.js
+++ tasks/grunt-cp.js
@@ -24,22 +24,12 @@ module.exports = function(grunt) {
   // ==========================================================================
 
   grunt.registerMultiTask('generate-aws', 'Generate AWS policy and signature.', function () {
-    //console.log("dest", )
+
     var options = this.options();
     var aws = new AWS(options);
     var filepath = process.cwd() + '/' + options.dest;
     console.log("File " + options.dest + " created.")
 
     return grunt.file.write(filepath, 'module.exports = ' + JSON.stringify(aws));
-
-    /*
-    this.files.forEach(function(file) {
-      return file.src.map(function(filepath) {
-        var definePath = (filepath.replace(/\.\w+$/, '')),
-            original = grunt.file.read(path.join(file.cwd, filepath));
-        return grunt.file.write(file.dest + filepath, 'window.require.define({"' + definePath + '": function(exports, require, module) {\n' + original + '}});\n\n');
-      });
-    });
-    */
   });
 }
